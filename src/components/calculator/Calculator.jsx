3b4f05942690b53fa728218c718b3f082b312b72
jjgorton <jjgorton@gmail.com>
2022-11-09 20:12:33 -0500

    reformat state to use objects for clarity

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,37 +1,59 @@
-import React from 'react'
+import React from "react";
 
-import { useState } from 'react'
+import { useState } from "react";
 
-import CurrentCD from '../currentCD/CurrentCD'
-import Penalty from '../penalty/Penalty'
-import OtherCD from '../otherCD/OtherCD'
+import CurrentCD from "../currentCD/CurrentCD";
+import Penalty from "../penalty/Penalty";
+import OtherCD from "../otherCD/OtherCD";
 
 const Calculator = () => {
+	// const [oldPrincipal, setOldPrincipal] = useState(0);
+	// const [oldRate, setOldRate] = useState(0);
+	// const [oldFreq, setOldFreq] = useState("daily");
+	// const [oldInterest, setOldInterest] = useState(0);
+	// const [term, setTerm] = useState(0);
+	const [oldCD, setOldCD] = useState({
+		principal: 0,
+		rate: 0,
+		freq: "daily",
+		interest: 0,
+		term: 0,
+	});
 
-    const [oldPrincipal, setOldPrincipal] = useState(0);
-    const [oldRate, setOldRate] = useState(0);
-    const [oldFreq, setOldFreq] = useState('daily');
-    const [term, setTerm] = useState(0);
-    const [oldInterest, setOldInterest] = useState(0)
+	const [penaltyTerm, setPenaltyTerm] = useState(0);
+	const [penaltySimple, setPenaltySimple] = useState(true);
+	const [penaltyFee, setPenaltyFee] = useState(0);
+	const [totalPenalty, setTotalPenalty] = useState(0);
 
-    const [penaltyTerm, setPenaltyTerm] = useState(0);
-    const [penaltySimple, setPenaltySimple] = useState(true);
-    const [penaltyFee, setPenaltyFee] = useState(0);
-    const [totalPenalty, setTotalPenalty] = useState(0)
+	const [otherPrincipal, setOtherPrincipal] = useState(0);
+	const [otherRate, setOtherRate] = useState(0);
+	const [otherInterest, setOtherInterest] = useState(0);
 
-    const [otherPrincipal, setOtherPrincipal] = useState(0)
-    const [otherRate, setOtherRate] = useState(0);
-    const [otherInterest, setOtherInterest] = useState(0);
+	return (
+		<>
+			<CurrentCD
+				oldPrincipal={oldPrincipal}
+				setOldPrincipal={setOldPrincipal}
+				oldRate={oldRate}
+				setOldRate={setOldRate}
+				oldFreq={oldFreq}
+				setOldFreq={setOldFreq}
+				term={term}
+				setTerm={setTerm}
+				oldInterest={oldInterest}
+				setOldInterest={setOldInterest}
+			/>
+			<Penalty />
+			<p>
+				{oldInterest > otherInterest
+					? `>`
+					: oldInterest < otherInterest
+					? `<`
+					: `=`}
+			</p>
+			<OtherCD />
+		</>
+	);
+};
 
-
-  return (
-    <>
-        <CurrentCD />
-        <Penalty />
-        <p>{oldInterest > otherInterest ? `>` : oldInterest < otherInterest ? `<` : `=`}</p>
-        <OtherCD />
-    </>
-  )
-}
-
-export default Calculator
+export default Calculator;
--- src/components/currentCD/CurrentCD.jsx
+++ src/components/currentCD/CurrentCD.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 
-const CurrentCD = () => {
+const CurrentCD = ({oldPrincipal,
+     setOldPrincipal,
+    oldRate,
+    setOldRate,
+    oldFreq,
+    setOldFreq,
+    term,
+    setTerm,
+    oldInterest,
+    setOldInterest}) => {
+
+      const handleChanges = (e) => {
+        
+      }
+
+
   return (
-    <div>CurrentCD</div>
+    <div>
+        <h1>Current CD</h1>
+        <form>
+            <input
+                type='number'
+                id='old-principal'
+                name='old principal'
+                value={oldPrincipal}
+                onChange={handleChanges}
+            />
+
+        </form>
+    </div>
   )
 }
 
